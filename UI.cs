888d3c5d34799946687f83173e6e406797d2be0e
OLeDouxEt <111026713+OLeDouxEt@users.noreply.github.com>
2022-09-13 22:55:45 -0500

    UI improvements and RSS feed file
    Added a UI class to handle most of the work displaying the interface selection and the news stories. Also added a config file for users to enter their desired RSS feeds for the application to read from.

---
+++ UI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMD_News
+{
+    public static class UI
+    {
+        public static string SourceSelect(List<string> rssList)
+        {
+            string returnable = "";
+            int selectInt = 0;
+            for (int i = 0; i < rssList.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {rssList[i]}");
+            }
+            Console.WriteLine("Select News Source: ");
+            string selection = Console.ReadLine();
+            try
+            {
+                selectInt = Int32.Parse(selection);
+                if (selectInt > 0 && selectInt <= rssList.Count)
+                {
+                    returnable = rssList[selectInt - 1];
+                    return returnable;
+                }
+                else
+                {
+                    Console.WriteLine("Selection is not on the list. Try again.");
+                    return SourceSelect(rssList);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Not a number. Select again");
+                return SourceSelect(rssList);
+            }
+        }
+
+        public static async Task DisplayNewsItems(string rssFeed)
+        {
+            string justXML = await DataController.Get_Feed(rssFeed);
+            List<NewsItem> newsParse = DataController.ParseXML(justXML);
+            foreach (NewsItem item in newsParse)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(item.title);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(item.date);
+                Console.WriteLine(item.link);
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("-------------------------------");
+            }
+        }
+    }
+}
